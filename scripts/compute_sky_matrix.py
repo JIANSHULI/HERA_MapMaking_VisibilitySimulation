51d87c547d4c2a0d77e73ed49b4835f952a52a29
Jeff Zheng <jeff_z@mit.edu>
2014-09-07 22:03:13 -0400

    lastest code that shows my new algorithm not working

--- scripts/compute_sky_matrix.py
+++ scripts/compute_sky_matrix.py
@@ -13,29 +13,58 @@ import healpy.pixelfunc as hpf
 import scipy.interpolate as si
 import omnical.calibration_omni as omni
 
-tlist = np.arange(16, 24, .05)
-infofile = '/home/omniscope/omnical/doc/redundantinfo_X5_q3x.bin'
+tlist = np.arange(16, 24, .1)
+
+infofile = '/home/omniscope/omnical/doc/redundantinfo_X5_q3x.bin'#'/home/omniscope/omnical/doc/redundantinfo_PSA128_26ba_6bu_08-15-2014.bin'#
 info = omni.read_redundantinfo(infofile)
 
-nside = 16
+nside = 32
+nside_target = 8
+inclusion_thresh = 1 #betweennnn 0 and 1. ubl/lambda must be this times nside_target less
+all_ubl = False
 bnside = 8
 pol = 'xx'
 freq = 160.
 
 vs = sv.Visibility_Simulator()
-vs.initial_zenith = np.array([0,45.2977*np.pi/180])#self.zenithequ
+if 'X5' in infofile and 'PSA' in infofile:
+    raise Exception('Cant tell if the iinfo is for PAPER or MITEoR')
+elif 'X5' in infofile:
+    lat_degree = 45.2977
+    infoubl = info['ubl'].dot([[1.5,0,0],[0,1.5,0],[0,0,0]])
+    beam_healpix = np.fromfile('/home/omniscope/simulate_visibilities/data/MWA_beam_in_healpix_horizontal_coor/nside=%i_freq=%i_%s.bin'%(bnside, freq, pol), dtype='float32')
+elif 'PSA' in infofile:
+    lat_degree = -(30.+ 43./60. + 17.5/3600.)
+    infoubl = info['ubl'].dot([[4,0,0],[0,15,0],[0,0,0]])
+    calfile = 'psa6240_v003'
+    ######get array  information and accurate ubl vectors
+    aa = ap.cal.get_aa(calfile, np.array([freq/1000.]))
 
-beam_healpix = np.fromfile('/home/omniscope/simulate_visibilities/data/MWA_beam_in_healpix_horizontal_coor/nside=%i_freq=%i_%s.bin'%(bnside, freq, pol), dtype='float32')
-#vs.import_beam(beam_healpix)
+    ######load beam
+    nsideB = 8
+    Bl = nsideB*3 - 1
+
+    beam_healpix = np.zeros(12*nsideB**2, dtype='float32')
+    if pol[0] != pol[-1]:
+        raise Exception('ERROR: polarization string %s not supported in the code.'%pol)
+    for i in range(12*nsideB**2):
+        beam_healpix[i] = aa[0].bm_response(sv.rotatez_matrix(-np.pi/2).dot(hpf.pix2vec(nsideB, i)), pol[0])[0]#in paper's bm_response convention, (x,y) = (0,1) points north.
+else:
+    raise Exception('Cant tell if the iinfo is for PAPER or MITEoR')
+
+vs.initial_zenith = np.array([0, lat_degree*np.pi/180])#self.zenithequ
 beam_heal_equ = np.array(sv.rotate_healpixmap(beam_healpix, 0, np.pi/2 - vs.initial_zenith[1], vs.initial_zenith[0]))
 timer = time.time()
 
-
-ubls = [ubl for ubl in 3.*info['ubl'] if la.norm(ubl) < (nside * 299.792458 / freq)]
+if all_ubl:
+    ubls = infoubl
+else:
+    ubls = [ubl for ubl in infoubl if la.norm(ubl) < inclusion_thresh * (nside_target * 299.792458 / freq)]
 print "%i UBLs to include"%len(ubls)
 
 if len(tlist)*len(ubls) < 12*nside**2:
-    raise Exception('Not enough degree of freedom! %i*%i<%i.'%(len(tlist), len(ubls), 12*nside**2))
+    for i in range(5):
+        print 'Not enough degree of freedom! %i*%i<%i.'%(len(tlist), len(ubls), 12*nside**2)
 A = np.empty((len(tlist)*len(ubls), 12*nside**2), dtype='complex64')
 for i in range(12*nside**2):
     dec, ra = hpf.pix2ang(nside, i)#gives theta phi
@@ -45,5 +74,8 @@ for i in range(12*nside**2):
 
     A[:, i] = np.array([vs.calculate_pointsource_visibility(ra, dec, d, freq, beam_heal_equ = beam_heal_equ, tlist = tlist) for d in ubls]).flatten()
 print float(time.time()-timer)/60.
-A.tofile('/home/omniscope/simulate_visibilities/data/Amatrix_' + '_nside%i_%iby%i_'%(nside, len(A), len(A[0])) + infofile.split('/')[-1])
+if all_ubl:
+    A.tofile('/home/omniscope/simulate_visibilities/data/Amatrix_allubl_nside%i_%iby%i_'%(nside, len(A), len(A[0])) + infofile.split('/')[-1])
+else:
+    A.tofile('/home/omniscope/simulate_visibilities/data/Amatrix_%iubl_nside%i_%iby%i_'%(len(ubls), nside, len(A), len(A[0])) + infofile.split('/')[-1])
 
--- scripts/investigate_skymatrix.py
+++ scripts/investigate_skymatrix.py
@@ -1,15 +1,24 @@
 import numpy as np
-import numpy.linalg as la
+import scipy.linalg as la
 import matplotlib.pyplot as plt
 import healpy.visufunc as hpv
 import healpy as hp
 import healpy.pixelfunc as hpf
 import sys
-
-#nside = 8
-#nUBL = 34
-nside = 16
-nUBL = 75
+import omnical.calibration_omni as omni
+nt = 80
+nside = 8
+nUBL = 75#34
+#nside = 16
+#nUBL = 75
+freq = 160.
+thresh = 1
+ubls = np.array([bl for bl in omni.read_redundantinfo('/home/omniscope/omnical/doc/redundantinfo_X5_q3x.bin')['ubl']*[1.5, 1.5, 0] if la.norm(bl) < thresh * (nside * 299.792458 / freq)])
+if len(ubls) != nUBL:
+    raise Exception('%i != %i!'%(len(ubls), nUBL))
+thresh2 = .6
+subublindex = np.array([u for u in range(nUBL) if la.norm(ubls[u]) < thresh2 * (nside * 299.792458 / freq)])
+print "%i out of %i to include"%(len(subublindex), nUBL)
 
 pca1 = hp.fitsfunc.read_map('/home/omniscope/simulate_visibilities/data/gsm1.fits' + str(nside))
 pca2 = hp.fitsfunc.read_map('/home/omniscope/simulate_visibilities/data/gsm2.fits' + str(nside))
@@ -24,55 +33,118 @@ for i in range(12*nside**2):
     equatorial_GSM[i] = hpf.get_interp_val(gsm, ang[0], ang[1])
 print "done."
 sys.stdout.flush()
-#################################plot actual errors and such
-#for nt in [40, 80]:
-    #A = np.fromfile('/home/omniscope/simulate_visibilities/data/Amatrix__nside%i_%iby%i_redundantinfo_X5_q3x.bin'%(nside, nt*nUBL, 12*nside**2),dtype='complex64').reshape((nt*nUBL, 12*nside**2))
-    ##hpv.mollview(equatorial_GSM, return_projected_map=True, min=0,max=5000,fig=1,title='Original map')
-    #vis = A.dot(equatorial_GSM)
-    #vis = vis + .01 * (2**-.5) * np.mean(np.abs(vis)) * (np.random.randn(len(vis)) + np.random.randn(len(vis)) * 1.j)
-    #solution = la.pinv(A.conjugate().transpose().dot(A)).dot(A.conjugate().transpose().dot(vis))
-    ##hpv.mollview(solution, return_projected_map=True, min=0,max=5000,fig=2,title='Our solution')
-    #hpv.mollview(np.log10(np.abs(solution-equatorial_GSM)/equatorial_GSM), return_projected_map=True, min=-3, max=0,title='log10(relative error)')
+
+
+nside_standard = 8#64
+nt_standard = 80#160
+pca1 = hp.fitsfunc.read_map('/home/omniscope/simulate_visibilities/data/gsm1.fits' + str(nside_standard))
+pca2 = hp.fitsfunc.read_map('/home/omniscope/simulate_visibilities/data/gsm2.fits' + str(nside_standard))
+pca3 = hp.fitsfunc.read_map('/home/omniscope/simulate_visibilities/data/gsm3.fits' + str(nside_standard))
+gsm_standard = 422.952*(0.307706*pca1+-0.281772*pca2+0.0123976*pca3)
+equatorial_GSM_standard = np.zeros(12*nside_standard**2,'float')
+#rotate sky map
+print "Rotating GSM_standard...",
+sys.stdout.flush()
+for i in range(12*nside_standard**2):
+    ang = hp.rotator.Rotator(coord='cg')(hpf.pix2ang(nside_standard,i))
+    equatorial_GSM_standard[i] = hpf.get_interp_val(gsm_standard, ang[0], ang[1])
+print "done."
+sys.stdout.flush()
+###################################plot actual errors and such, see if the gridding is too coarse
+
+A = np.fromfile('/home/omniscope/simulate_visibilities/data/Amatrix_%iubl_nside%i_%iby%i_redundantinfo_X5_q3x.bin'%(nUBL, nside, nt*nUBL, 12*nside**2),dtype='complex64').reshape((nUBL, nt, 12*nside**2))[subublindex].reshape((len(subublindex) * nt, 12*nside**2))/nside**2
+
+#ublindex = [u for u in range(len(ubls_standard)) if la.norm(ubls_standard[u]) < (nside * 299.792458 / freq)]
+A_standard = np.fromfile('/home/omniscope/simulate_visibilities/data/Amatrix_%iubl_nside%i_%iby%i_redundantinfo_X5_q3x.bin'%(nUBL, nside_standard, nt_standard*nUBL, 12*nside_standard**2),dtype='complex64').reshape((nUBL, nt_standard, 12*nside_standard**2))[subublindex, ::(nt_standard/nt)].reshape((len(subublindex)*nt, 12*nside_standard**2))/nside_standard**2
+
+vis = A_standard.dot(equatorial_GSM_standard)
+visold = A.dot(equatorial_GSM)
+plt.plot(np.real(vis))
+plt.plot(np.real(visold))
+plt.show()
+#quit()
+vis = vis + .01 * (2**-.5) * np.mean(np.abs(vis)) * (np.random.randn(len(vis)) + np.random.randn(len(vis)) * 1.j)
+print vis.shape, A.shape
+sys.stdout.flush()
+solution = la.pinv(A.conjugate().transpose().dot(A), rcond = 1e-10).dot(A.conjugate().transpose().dot(vis))
+hpv.mollview(equatorial_GSM, return_projected_map=True, min=0,max=5000,fig=1,title='Original map')
+hpv.mollview(solution, return_projected_map=True, min=0,max=5000,fig=2,title='Our solution')
+hpv.mollview(np.log10(np.abs(solution-equatorial_GSM)/equatorial_GSM), return_projected_map=True, min=-3, max=0,title='log10(relative error)')
+plt.show()
+
+#################################plot actual errors and such, see if adding PAPER PSA128 helps
+#nPAPERUBL = 13
+#A = np.concatenate((np.fromfile('/home/omniscope/simulate_visibilities/data/Amatrix__nside%i_%iby%i_redundantinfo_X5_q3x.bin'%(nside, nt*nUBL, 12*nside**2),dtype='complex64').reshape((nt*nUBL, 12*nside**2)), np.fromfile('/home/omniscope/simulate_visibilities/data/Amatrix__nside%i_%iby%i_redundantinfo_PSA128_26ba_6bu_08-15-2014.bin'%(nside, nt*nPAPERUBL, 12*nside**2),dtype='complex64').reshape((nt*nPAPERUBL, 12*nside**2))))
+
+#hpv.mollview(equatorial_GSM, return_projected_map=True, min=0,max=5000,fig=1,title='Original map')
+#vis = A.dot(equatorial_GSM)
+#vis = vis + .01 * (2**-.5) * np.mean(np.abs(vis)) * (np.random.randn(len(vis)) + np.random.randn(len(vis)) * 1.j)
+#solution = la.pinv(A.conjugate().transpose().dot(A), rcond = 1e-10).dot(A.conjugate().transpose().dot(vis))
+#hpv.mollview(solution, return_projected_map=True, min=0,max=5000,fig=2,title='Our solution')
+#hpv.mollview(np.log10(np.abs(solution-equatorial_GSM)/equatorial_GSM), return_projected_map=True, min=-3, max=0,title='log10(relative error)')
 #plt.show()
 
 ###################################investigate eigen values
-nt = 80
-A = np.fromfile('/home/omniscope/simulate_visibilities/data/Amatrix__nside%i_%iby%i_redundantinfo_X5_q3x.bin'%(nside, nt*nUBL, 12*nside**2),dtype='complex64').reshape((nt*nUBL, 12*nside**2))
-print "Computing eigenvalues for A...",
-sys.stdout.flush()
-wA,_ = la.eigh(A.conjugate().transpose().dot(A))
-print "done."
-sys.stdout.flush()
 
-thresh = 0.0005
-maxnorm = np.max([la.norm(v) for v in A.transpose()])
-print "max norm", maxnorm
-B = np.array([v for v in A.transpose() if la.norm(v) > maxnorm*thresh]).transpose()
-valid_pix = [i for i in range(len(A[0])) if la.norm(A[:,i]) > maxnorm*thresh]
-print "number of pixels over thresh %.1f"%(maxnorm*thresh), len(B[0])
+#A = np.fromfile('/home/omniscope/simulate_visibilities/data/Amatrix__nside%i_%iby%i_redundantinfo_X5_q3x.bin'%(nside, nt*nUBL, 12*nside**2),dtype='complex64').reshape((nt*nUBL, 12*nside**2))
+#print "Computing eigenvalues for A...",
+#sys.stdout.flush()
+#wA,_ = la.eigh(A.conjugate().transpose().dot(A))
+#print "done."
+#sys.stdout.flush()
 
-print "Computing eigenvalues for B...",
-sys.stdout.flush()
-wB,_ = la.eigh(B.conjugate().transpose().dot(B))
-print "condition", np.abs(wB[0])/np.abs(wB[-1])
-sys.stdout.flush()
+#thresh = 0.0005
+#maxnorm = np.max([la.norm(v) for v in A.transpose()])
+#print "max norm", maxnorm
+#B = np.array([v for v in A.transpose() if la.norm(v) > maxnorm*thresh]).transpose()
+#valid_pix = [i for i in range(len(A[0])) if la.norm(A[:,i]) > maxnorm*thresh]
+#print "number of pixels over thresh %.1f"%(maxnorm*thresh), len(B[0])
 
-vis = A.dot(equatorial_GSM)
-noise = .01 * (2**-.5) * np.mean(np.abs(vis)) * (np.random.randn(len(vis)) + np.random.randn(len(vis)) * 1.j)
-vis = vis + noise
-solution = np.zeros(12*nside**2, dtype='complex64')
-solution_noise = np.zeros(12*nside**2)
-BtBinv = la.pinv(B.conjugate().transpose().dot(B), rcond=1e-10)
-solution[valid_pix] = BtBinv.dot(B.conjugate().transpose().dot(vis))
-solution_noise[valid_pix] = BtBinv.dot(B.conjugate().transpose().dot(noise))
-hpv.mollview(np.log10(equatorial_GSM), return_projected_map=True, min=2.5,max=4,title='Original map')
-hpv.mollview(np.log10(np.real(solution)), return_projected_map=True, min=2.5,max=4,title='Solution_real')
-#hpv.mollview(np.log10(np.imag(solution)), return_projected_map=True, min=2.5,max=4,title='Solution_imag')
-hpv.mollview(np.log10(np.abs(solution_noise)), return_projected_map=True, min=0.5,max=2,title='Solution noise')
-hpv.mollview(np.log10(np.abs(solution-equatorial_GSM)/equatorial_GSM), return_projected_map=True, min=-3, max=0,title='log10(relative error) for thresh %.3f'%thresh)
-plt.show()
+#print "Computing eigenvalues for B...",
+#sys.stdout.flush()
+#wB,_ = la.eigh(B.conjugate().transpose().dot(B))
+#print "condition", np.abs(wB[0])/np.abs(wB[-1])
+#sys.stdout.flush()
 
-#plt.plot(np.abs(wA)/np.abs(wA)[0])
-#plt.plot(np.abs(wB)/np.abs(wB)[0])
-plt.imshow(np.log10(np.abs(BtBinv)/np.max(np.abs(BtBinv))))
-plt.show()
+#vis = A.dot(equatorial_GSM)
+#noise = .01 * (2**-.5) * np.mean(np.abs(vis)) * (np.random.randn(len(vis)) + np.random.randn(len(vis)) * 1.j)
+#vis = vis + noise
+#solution = np.zeros(12*nside**2, dtype='complex64')
+#solution_noise = np.zeros(12*nside**2)
+#BtBinv = la.pinv(B.conjugate().transpose().dot(B), rcond=1e-15)
+#solution[valid_pix] = BtBinv.dot(B.conjugate().transpose().dot(vis))
+#solution_noise[valid_pix] = BtBinv.dot(B.conjugate().transpose().dot(noise))
+#hpv.mollview(np.log10(equatorial_GSM), return_projected_map=True, min=2.5,max=4,title='Original map')
+#hpv.mollview(np.log10(np.real(solution)), return_projected_map=True, min=2.5,max=4,title='Solution_real')
+##hpv.mollview(np.log10(np.imag(solution)), return_projected_map=True, min=2.5,max=4,title='Solution_imag')
+#hpv.mollview(np.log10(np.abs(solution_noise)), return_projected_map=True, min=0.5,max=2,title='Solution noise')
+#hpv.mollview(np.log10(np.abs(solution-equatorial_GSM)/equatorial_GSM), return_projected_map=True, min=-3, max=0,title='log10(relative error) for thresh %.3f'%thresh)
+#plt.show()
+
+##plt.plot(np.abs(wA)/np.abs(wA)[0])
+##plt.plot(np.abs(wB)/np.abs(wB)[0])
+
+##for i in range(len(BtBinv)):
+    ##BtBinv[i,:] = BtBinv[i,:] / BtBinv[i,i]**.5
+    ##BtBinv[:,i] = BtBinv[:,i] / BtBinv[i,i]
+#tmp = BtBinv
+#for i in range(len(BtBinv)):
+    #for j in range(len(BtBinv)):
+        #BtBinv[i,j] = tmp[i,j] / tmp[i,i]**.5 / tmp[j,j]**.5
+
+#plt.subplot('131')
+#plot = plt.imshow(np.log10(np.abs(BtBinv)))
+#plot.set_clim(-2,0)
+
+#plt.subplot('132')
+#plot = plt.imshow(np.log10(np.real(BtBinv)))
+#plot.set_clim(-2,0)
+
+#plt.subplot('133')
+#plot = plt.imshow(np.log10(np.imag(BtBinv)))
+#plt.colorbar()
+#plot.set_clim(-2,0)
+#plt.show()
+#plt.imshow(np.imag(np.log10(BtBinv)))
+#plt.colorbar()
+#plt.show()
